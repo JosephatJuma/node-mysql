0ae577af8880c8265079f024c0ca981ff8ffa6de
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-20 19:12:11 +0300

    separeded the app in src

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //imported routes
 const agricOfficer = require("./routes/agric-officer");
-
 app.use("/agric-officers", agricOfficer);
 
 app.listen(port, () => {
--- src/routes/agric-officer.js
+++ src/routes/agric-officer.js
@@ -5,6 +5,9 @@ router.get("/", async (req, res) => {
   res.send("list of all agric officers");
 });
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
 
 module.exports = router;

51a0fce623df7429615a64434b5ba99facf9852d
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-21 17:41:02 +0300

    deployment

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-const mysql = require("mysql2");
+const mysql = require("mysql2"); //npm i mysql2
 
 dotenv.config();
 const database = mysql
@@ -7,9 +7,9 @@ const database = mysql
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    database: "railway",
     port: process.env.DATABASEPORT,
-    connectTimeout: 60000, // Set connection timeout to 30 seconds
+    connectTimeout: 60000, // Set connection timeout to 60 seconds
   })
   .promise();
 

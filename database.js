45602767bffdc27b6aa2ca176724035c9cf3762a
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-21 08:34:30 +0300

    db configuration

--- database.js
+++ database.js
@@ -1,3 +1,15 @@
 const dotenv = require("dotenv");
+const mysql = require("mysql2");
 
 dotenv.config();
+const connect = mysql
+  .createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT,
+  })
+  .promise();
+
+module.exports = connect;
